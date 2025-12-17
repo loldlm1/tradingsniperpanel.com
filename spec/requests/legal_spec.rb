154ec015a096210a77fb8e94f29959bc90036b19
Moises T <loldlm1@gmail.com>
2025-12-17 09:47:33 -0400

    Fixed the whole neon landing page

--- spec/requests/legal_spec.rb
+++ spec/requests/legal_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe "Legal pages", type: :request do
     get terms_path
     expect(response).to have_http_status(:ok)
     expect(response.body).to include(I18n.t("legal.terms.title"))
+    first_terms_section = Array(I18n.t("legal.terms.sections", default: [])).first&.with_indifferent_access
+    first_terms_paragraph = Array(first_terms_section&.dig(:paragraphs)).first
+    expect(response.body).to include(first_terms_paragraph) if first_terms_paragraph.present?
 
     get privacy_path
     expect(response).to have_http_status(:ok)
     expect(response.body).to include(I18n.t("legal.privacy.title"))
+    first_privacy_section = Array(I18n.t("legal.privacy.sections", default: [])).first&.with_indifferent_access
+    first_privacy_paragraph = Array(first_privacy_section&.dig(:paragraphs)).first
+    expect(response.body).to include(first_privacy_paragraph) if first_privacy_paragraph.present?
   end
 
   it "does not redirect signed-in users from terms and privacy" do
@@ -23,4 +29,3 @@ RSpec.describe "Legal pages", type: :request do
     expect(response).to have_http_status(:ok)
   end
 end
-
