10d0e6e094bb1b062edd24df47cc309784b09fa8
Moises T <loldlm1@gmail.com>
2025-12-19 15:09:59 -0400

    fixed tests and role

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,6 +1,17 @@
 require "omniauth"
+require "logger"
+require "stringio"
 
 OmniAuth.config.test_mode = true
+OmniAuth.config.logger = Logger.new(StringIO.new)
+OmniAuth.config.mock_auth[:google_oauth2] ||= OmniAuth::AuthHash.new(
+  provider: "google_oauth2",
+  uid: "test-uid",
+  info: {
+    email: "test-user@example.com",
+    name: "Test User"
+  }
+)
 
 module OmniauthHelpers
   def mock_google_oauth(email: "google.user@example.com", uid: "google-uid-123", name: "Google User")
