512fbbe6145c1644919491fd9592e93ee4d1e90b
Moises T <loldlm1@gmail.com>
2025-12-27 13:37:32 -0400

    added stripe call

--- app/services/billing/invoice_plan_labeler.rb
+++ app/services/billing/invoice_plan_labeler.rb
@@ -6,25 +6,32 @@ module Billing
     end
 
     def label_for(invoice, fallback_label: nil)
-      cached = cached_plan_keys(invoice)
-      label = label_from_keys(cached)
-      return label if label.present?
+      return fallback_label unless stripe_enabled?
 
-      keys = plan_keys_from_invoice(invoice)
-      label = label_from_keys(keys)
-      if label.present?
-        cache_plan_keys(invoice, keys)
-        return label
+      stripe_invoice_id = stripe_invoice_id(invoice)
+      return fallback_label if stripe_invoice_id.blank?
+
+      stripe_invoice = fetch_stripe_invoice(stripe_invoice_id)
+      return fallback_label if stripe_invoice.blank?
+
+      price_totals = price_totals_for(stripe_invoice)
+      return fallback_label if price_totals.blank?
+
+      if price_totals.size == 1
+        price_key = price_totals.keys.first
+        return plan_label_for(price_key) || fallback_label
       end
 
-      keys = plan_keys_from_stripe(invoice)
-      label = label_from_keys(keys)
-      if label.present?
-        cache_plan_keys(invoice, keys)
-        return label
-      end
+      from_key, to_key = select_change_keys(price_totals)
+      return fallback_label if from_key.blank? || to_key.blank?
 
-      fallback_label
+      change_label = change_label_for(from_key, to_key)
+      I18n.t(
+        "dashboard.billing.invoice_plan_change",
+        from: plan_label_for(from_key),
+        to: plan_label_for(to_key),
+        change: change_label
+      )
     rescue StandardError => e
       logger.warn("[Billing::InvoicePlanLabeler] failed invoice_id=#{invoice_id(invoice)}: #{e.class} - #{e.message}")
       fallback_label
@@ -34,7 +41,7 @@ module Billing
 
     attr_reader :pricing_catalog, :logger
 
-    def plan_keys_from_invoice(invoice)
+    def price_totals_for(invoice)
       lines = invoice_lines(invoice)
       return {} if lines.blank?
 
@@ -46,42 +53,14 @@ module Billing
         totals[price_key] += line_amount(line)
       end
 
-      build_plan_keys(totals.compact_blank)
-    end
-
-    def plan_keys_from_stripe(invoice)
-      return {} unless stripe_enabled?
-
-      stripe_invoice_id = stripe_invoice_id(invoice)
-      return {} if stripe_invoice_id.blank?
-
-      stripe_invoice = fetch_stripe_invoice(stripe_invoice_id)
-      return {} if stripe_invoice.blank?
-
-      lines = invoice_lines(stripe_invoice)
-      return {} if lines.blank?
-
-      totals = Hash.new(0)
-      lines.each do |line|
-        price_key = price_key_for_line(line)
-        next if price_key.blank?
-
-        totals[price_key] += line_amount(line)
-      end
-
-      keys = build_plan_keys(totals.compact_blank)
-      cache_stripe_invoice(invoice, stripe_invoice)
-      keys
+      totals.compact_blank
     end
 
     def invoice_lines(invoice)
-      invoice_object = stripe_invoice_object(invoice)
-      return [] if invoice_object.blank?
-
-      lines = if invoice_object.respond_to?(:lines)
-                invoice_object.lines
-              elsif invoice_object.is_a?(Hash)
-                invoice_object["lines"] || invoice_object[:lines]
+      lines = if invoice.respond_to?(:lines)
+                invoice.lines
+              elsif invoice.is_a?(Hash)
+                invoice["lines"] || invoice[:lines]
               end
 
       return [] if lines.blank?
@@ -93,17 +72,6 @@ module Billing
       else
         Array(lines)
       end
-    end
-
-    def stripe_invoice_object(invoice)
-      return invoice if stripe_invoice?(invoice)
-
-      if invoice.respond_to?(:stripe_invoice) && invoice.stripe_invoice.present?
-        return invoice.stripe_invoice
-      end
-
-      data = invoice.respond_to?(:data) ? invoice.data : nil
-      data&.dig("stripe_invoice") || data&.dig(:stripe_invoice)
     end
 
     def price_key_for_line(line)
@@ -157,15 +125,7 @@ module Billing
                line["plan"] || line[:plan]
              end
 
-      return if plan.blank?
-
-      if plan.respond_to?(:id)
-        plan.id
-      elsif plan.is_a?(Hash)
-        plan["id"] || plan[:id]
-      else
-        plan
-      end
+      extract_id(plan)
     end
 
     def plan_product_id_for(line)
@@ -194,19 +154,6 @@ module Billing
                end
 
       amount.to_i
-    end
-
-    def build_plan_keys(price_totals)
-      return {} if price_totals.blank?
-
-      if price_totals.size == 1
-        return { single_key: price_totals.keys.first }
-      end
-
-      from_key, to_key = select_change_keys(price_totals)
-      return {} if from_key.blank? || to_key.blank?
-
-      { from_key: from_key, to_key: to_key }
     end
 
     def select_change_keys(price_totals)
@@ -252,82 +199,10 @@ module Billing
       invoice.respond_to?(:id) ? invoice.id : nil
     end
 
-    def cached_plan_keys(invoice)
-      data = invoice.respond_to?(:data) ? invoice.data : nil
-      return {} if data.blank?
-
-      if data["invoice_plan_key"].present?
-        return { single_key: data["invoice_plan_key"] }
-      end
-
-      from_key = data["invoice_plan_from_key"]
-      to_key = data["invoice_plan_to_key"]
-      return {} if from_key.blank? || to_key.blank?
-
-      { from_key: from_key, to_key: to_key }
-    end
-
-    def cache_plan_keys(invoice, keys)
-      return unless invoice.respond_to?(:update!)
-
-      data = invoice.respond_to?(:data) ? invoice.data : nil
-      data = data.is_a?(Hash) ? data : {}
-      update = data.dup
-
-      if keys[:single_key].present?
-        update["invoice_plan_key"] = keys[:single_key]
-        update.delete("invoice_plan_from_key")
-        update.delete("invoice_plan_to_key")
-      elsif keys[:from_key].present? && keys[:to_key].present?
-        update["invoice_plan_from_key"] = keys[:from_key]
-        update["invoice_plan_to_key"] = keys[:to_key]
-        update.delete("invoice_plan_key")
-      else
-        return
-      end
-
-      invoice.update!(data: update)
-    rescue StandardError => e
-      logger.warn("[Billing::InvoicePlanLabeler] cache failed invoice_id=#{invoice_id(invoice)}: #{e.class} - #{e.message}")
-    end
-
-    def cache_stripe_invoice(invoice, stripe_invoice)
-      return unless invoice.respond_to?(:update!)
-
-      data = invoice.respond_to?(:data) ? invoice.data : nil
-      data = data.is_a?(Hash) ? data : {}
-      update = data.merge("stripe_invoice" => stripe_invoice.to_hash)
-      invoice.update!(data: update)
-    rescue StandardError => e
-      logger.warn("[Billing::InvoicePlanLabeler] stripe invoice cache failed invoice_id=#{invoice_id(invoice)}: #{e.class} - #{e.message}")
-    end
-
-    def label_from_keys(keys)
-      return if keys.blank?
-
-      if keys[:single_key].present?
-        return plan_label_for(keys[:single_key])
-      end
-
-      from_key = keys[:from_key]
-      to_key = keys[:to_key]
-      return if from_key.blank? || to_key.blank?
-
-      change_label = change_label_for(from_key, to_key)
-      I18n.t(
-        "dashboard.billing.invoice_plan_change",
-        from: plan_label_for(from_key),
-        to: plan_label_for(to_key),
-        change: change_label
-      )
-    end
-
-    def stripe_invoice?(invoice)
-      invoice.respond_to?(:lines) && invoice.respond_to?(:id)
-    end
-
     def stripe_invoice_id(invoice)
-      return invoice.id if stripe_invoice?(invoice)
+      if invoice.respond_to?(:stripe_invoice) && invoice.stripe_invoice.present?
+        return invoice.stripe_invoice.id if invoice.stripe_invoice.respond_to?(:id)
+      end
 
       data = invoice.respond_to?(:data) ? invoice.data : nil
       return if data.blank?
@@ -335,11 +210,7 @@ module Billing
       stripe_invoice = data["stripe_invoice"] || data[:stripe_invoice]
       return if stripe_invoice.blank?
 
-      if stripe_invoice.respond_to?(:id)
-        stripe_invoice.id
-      elsif stripe_invoice.is_a?(Hash)
-        stripe_invoice["id"] || stripe_invoice[:id]
-      end
+      extract_id(stripe_invoice)
     end
 
     def fetch_stripe_invoice(stripe_invoice_id)
