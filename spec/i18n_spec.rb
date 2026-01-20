e15fab8907d34e8348318891efb844276e92eabe
Moises T <loldlm1@gmail.com>
2026-01-20 14:40:22 -0400

    improved tests on the dashboard

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe "I18n translations" do
       expect { I18n.t!("dashboard.courses.index.title", locale:) }.not_to raise_error
       expect { I18n.t!("dashboard.courses.unlock_cta", locale:) }.not_to raise_error
       expect { I18n.t!("dashboard.nav.courses", locale:) }.not_to raise_error
+      expect { I18n.t!("dashboard.main.header.title", locale:) }.not_to raise_error
+      expect { I18n.t!("dashboard.main.pnl_card.title", locale:) }.not_to raise_error
+      expect { I18n.t!("dashboard.main.balance_card.title", locale:) }.not_to raise_error
+      expect { I18n.t!("dashboard.main.account_summary.title", locale:) }.not_to raise_error
       expect { I18n.t!("active_admin.marketplace_products.sections.product", locale:) }.not_to raise_error
       expect { I18n.t!("active_admin.marketplace_assets.labels.marketplace_products", locale:) }.not_to raise_error
       expect { I18n.t!("active_admin.expert_advisors.bundle_coverage.complete", locale:) }.not_to raise_error
